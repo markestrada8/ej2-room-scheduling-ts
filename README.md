123951f5bd008655367445f1cadef31831a5b033
Mark Estrada <mark.estrada.dev>
2022-07-07 08:32:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Creating a Meeting Room Calendar Using the Syncfusion JavaScript Scheduler
+
+A meeting room calendar displays a list of conference rooms in a corporate office with quick overview to the available rooms. Other than the reserved time slots for meeting purpose, the common unavailable timings such as lunch break and maintenance works of those meeting rooms will be blocked and unaccessible to anyone. This application has been built completely by making use of the [quick start](https://github.com/syncfusion/ej2-quickstart.git) TypeScript project.
+
+![Meeting Room Calendar](meeting-room.png)
+
+It has been developed by making use of the [multiple resources](https://ej2.syncfusion.com/documentation/schedule/resources.html?lang=typescript) feature of scheduler component.
+
+## Calendar functionalities
+
+The multiple resources functionalities incorporated in this sample application are,
+
+* **[resources](https://ej2.syncfusion.com/documentation/schedule/resources.html?lang=typescript)** - Defines the resource data. Here, the meeting room data is provided as dataSource.
+* **[group](https://ej2.syncfusion.com/demos/#/material/schedule/group.html)** - Allows the scheduler layout to group the resources physically. Here, 5 rooms are displayed visually on the same scheduler.
+* **[byDate](https://ej2.syncfusion.com/demos/#/material/schedule/group-bydate.html)** - Allows grouping of all the meeting rooms date-wise.
+* **[resourceHeaderTemplate](https://ej2.syncfusion.com/demos/#/material/schedule/group-custom-work-days.html)** - Allows customization of resource header cells. Here, the header row is customized by displaying the room names accompanied with its capacity and type.
+* **[allowGroupEdit](https://ej2.syncfusion.com/demos/#/material/schedule/group-editing.html)** - Allows the same appointment to be shared by more than one resource. Here, the `lunch break` and `maintenance` appointments are the grouped one and are made read-only.
+* **[Client-side events](https://ej2.syncfusion.com/demos/#/material/schedule/events.html)** - Events such as `actionBegin`, `renderCell`, `popupOpen` and `eventRendered` has been used in this application for achieving major customization of scheduler as well as in achieving the room reservation functionality. 
+
+## How to run this application?
+
+To run this application, you need to first clone the `ej2-schedule-meeting-room-calendar` repository and then navigate to its appropriate path where it has been located in your system.
+
+To do so, open the command prompt and run the below commands one after the other.
+
+```
+git clone https://github.com/SyncfusionSamples/ej2-schedule-meeting-room-calendar meeting-room
+cd meeting-room
+```
+
+## Installing
+
+Once done with downloading, next you need to install the necessay packages required to run this application locally. The `npm install` command will install all the needed JS2 packages into your current project and to do so, run the below command.
+
+```
+npm install
+```
+
+## Running
+
+Finally, you can now use the below `npm` script to run the web application.
+
+```
+npm start
+```
+
+As this application is configured with `browser-sync`, therefore it will serve the web application directly in your default browser.
+
+
---
+++ src/app/app.ts
@@ -0,0 +1,123 @@
+import {
+    Schedule, ScheduleModel, Day, ResourceDetails, 
+    PopupOpenEventArgs, RenderCellEventArgs, EventRenderedArgs, ActionEventArgs, CellClickEventArgs
+} from '@syncfusion/ej2-schedule';
+import { roomData } from './datasource';
+import { addClass, createElement, extend, isNullOrUndefined } from '@syncfusion/ej2-base';
+import { DropDownList } from '@syncfusion/ej2-dropdowns';
+
+Schedule.Inject(Day);
+
+interface TemplateFunction extends Window {
+    getRoomCapacity?: Function;
+    getRoomName?: Function;
+    getRoomType?: Function;
+}
+
+let isReadOnly: Function = (endDate: Date): boolean => {
+    return (endDate < new Date(2018, 6, 31, 0, 0));
+};
+
+let data: Object[] = <Object[]>extend([], roomData, null, true);
+let scheduleOptions: ScheduleModel = {
+        width: '100%',
+        height: '850px',
+        currentView: "Day",
+        selectedDate: new Date(2018, 6, 31),
+        resourceHeaderTemplate: '#resourceTemplate',
+        showWeekend: false,
+        group: {
+            resources: ['Rooms'],
+            byDate: true,
+            enableCompactView: false,
+            allowGroupEdit: true
+        },
+        workHours: { start: '08:00' },
+        resources: [{
+            field: 'RoomId', title: 'Select Room', name: 'Rooms', allowMultiple: true,
+            dataSource: [
+                { text: 'Jammy Cool', id: 1, capacity: 20, type: 'Conference' },
+                { text: 'Tweety Nest', id: 2, capacity: 7, type: 'Cabin'},
+                { text: 'Rounded Corner', id: 3, capacity: 5, type: 'Cabin' },
+                { text: 'Scenic View Hall', id: 4, capacity: 15, type: 'Conference' },
+                { text: 'Mission Hall', id: 5, capacity: 25, type: 'Conference' }
+            ],
+            textField: 'text', idField: 'id'
+        }],
+        views: ['Day'], 
+        eventSettings:{
+            dataSource: data,
+            enableTooltip: true,
+            tooltipTemplate: '#tooltipTemplate',
+            fields: {
+                id: 'Id',
+                subject: { title: 'Summary', name: 'Subject' },
+                location: { title: 'Location', name: 'Location' },
+                description: { title: 'Comments', name: 'Description' },
+                startTime: { title: 'From', name: 'StartTime' },
+                endTime: { title: 'To', name: 'EndTime' }
+            }
+        },
+        popupOpen: (args: PopupOpenEventArgs) => {
+            let data: any = <any>args.data;
+            if(args.type === "QuickInfo" || args.type === "Editor" || args.type === "RecurrenceAlert" || args.type === "DeleteAlert"){
+                let target: HTMLElement = (args.type == "RecurrenceAlert" || args.type == "DeleteAlert") ? data.element[0] : args.target;
+                if(!isNullOrUndefined(target) && target.classList.contains('e-work-cells')){
+                    let endDate = data.endTime as Date;
+                    let startDate = data.startTime as Date;
+                    let groupIndex = data.groupIndex as number;
+                    if ((target.classList.contains('e-read-only-cells')) || (!scheduleObj.isSlotAvailable(startDate as Date, endDate as Date, groupIndex as number))) {
+                        args.cancel = true;
+                    }
+                }
+                else if(target.classList.contains('e-appointment') && (isReadOnly(data.EndTime) || target.classList.contains('e-lunch-break') || target.classList.contains('e-maintenance'))){
+                    args.cancel=true;
+                }
+            }
+        },
+        renderCell: (args: RenderCellEventArgs) => {
+            if (args.element.classList.contains('e-work-cells')) {
+                // To disable the past date cells
+                if(args.date < new Date(2018, 6, 31, 0, 0)) {
+                    args.element.setAttribute('aria-readonly', 'true');
+                    args.element.classList.add('e-read-only-cells');
+                }
+            }
+        },
+        eventRendered: (args: EventRenderedArgs) => {
+            let data: any = <any>args.data;
+            if (isReadOnly(data.EndTime) || data.EventType == "Lunch" || data.EventType == "Maintenance") {
+                args.element.setAttribute('aria-readonly', 'true');
+                args.element.classList.add('e-read-only');
+            }
+            if(data.EventType == "Lunch"){
+                args.element.classList.add('e-lunch-break');
+            }
+            else if(data.EventType == "Maintenance"){
+                args.element.classList.add('e-maintenance');
+            }
+        },
+        actionBegin: (args: ActionEventArgs) => {
+            if(args.requestType == "eventCreate" || args.requestType == "eventChange"){
+                let data: any = <any>args.data;
+                let groupIndex = scheduleObj.eventBase.getGroupIndexFromEvent(data);
+                if(!scheduleObj.isSlotAvailable(data.StartTime as Date, data.EndTime as Date, groupIndex as number)) {
+                    args.cancel = true;
+                }
+            }
+        }
+    };
+
+    let scheduleObj: Schedule = new Schedule(scheduleOptions, document.getElementById('Schedule'));
+
+    (window as TemplateFunction).getRoomName = (value: ResourceDetails) => {
+        return (value as ResourceDetails).resourceData[(value as ResourceDetails).resource.textField];
+    };
+
+    (window as TemplateFunction).getRoomCapacity = (value: ResourceDetails) => {
+        return (value as any).resourceData.capacity;
+    };
+
+    (window as TemplateFunction).getRoomType = (value: ResourceDetails) => {
+        return (value as any).resourceData.type;
+    };
